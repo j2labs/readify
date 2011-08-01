5e963471ae12c7668ac35883f8de4140fa9ad36b
James Dennis <jd@j2labs.net>
2011-08-01 02:24:48 -0400

    Corrected login url in logout handler

--- handlers.py
+++ handlers.py
@@ -171,7 +171,7 @@ class AccountLogoutHandler(BaseHandler, Jinja2Rendering):
         """Clears cookie and sends user to login page
         """
         self.delete_cookies()
-        return self.redirect(self.login_url)
+        return self.redirect(self.application.login_url)
 
 
 ###
