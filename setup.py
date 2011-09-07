ba9cfef4e1cde200c2087ce9865b958a90514760
James Dennis <jd@j2labs.net>
2011-09-07 15:30:30 -0400

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='readify',
+      description='Example Brubeck project'
+      author='James Dennis',
+      author_email='jdennis@gmail.com',
+      url='http://github.com/j2labs/readify',
+      packages=['readify'])
