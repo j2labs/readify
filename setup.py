dd302ac36be1905b28cb246981dbd1421bbb7728
James Dennis <jd@j2labs.net>
2011-09-07 15:34:41 -0400

    Typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
  
 setup(name='readify',
-      description='Example Brubeck project'
+      description='Example Brubeck project',
       author='James Dennis',
       author_email='jdennis@gmail.com',
       url='http://github.com/j2labs/readify',
